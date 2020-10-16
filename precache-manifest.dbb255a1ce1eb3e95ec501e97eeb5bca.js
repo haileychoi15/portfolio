404818e410dd901a35b3f6309d6831ba7e8b97ec
yuyeong choi <imdud0612@gmail.com>
2020-10-16 15:22:14 +0900

    Updates

---
+++ precache-manifest.dbb255a1ce1eb3e95ec501e97eeb5bca.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c6bf44a5752f108c33e105a9ffca94d",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "fff6d70ad692de38adfe",
+    "url": "/portfolio/static/js/2.9bad499b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.9bad499b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa7d852bd47226932403",
+    "url": "/portfolio/static/js/main.f921c997.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  }
+]);
